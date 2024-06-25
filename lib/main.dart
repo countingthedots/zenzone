0635a458ae43b34007ddc977798f28d626d9ba81
countingthedots <countingthedots@gmail.com>
2024-06-25 10:30:36 +0200

    make gesture bar transparent

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:zenzone/application/getter.dart';
 import 'package:zenzone/application/router.dart';
@@ -29,14 +30,36 @@ void main() async{
   runApp(const ZenZone());
 }
 
-class ZenZone extends StatelessWidget {
+class ZenZone extends StatefulWidget {
   const ZenZone({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp.router(
+  State<ZenZone> createState() => _ZenZoneState();
+}
+
+class _ZenZoneState extends State<ZenZone> {
+@override
+void initState() {
+  super.initState();
+  WidgetsBinding.instance.addPostFrameCallback((_) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+  });
+}
+
+  @override
+  Widget build(BuildContext context) {
+      SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        systemNavigationBarColor: Color.fromARGB(255, 231, 203, 154), // Navigation bar
+        systemNavigationBarIconBrightness: Brightness.dark, // Navigation bar icons' brightness
+      ));
+      return MaterialApp.router(
         title: 'ZenZone',
         routerDelegate: goRouter.routerDelegate,
         routeInformationParser: goRouter.routeInformationParser,
         routeInformationProvider: goRouter.routeInformationProvider,
       );
+  }
 }
